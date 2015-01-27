f470def5f14de47e8b57cc3db1137143d6842df1
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-01-27 23:58:46 +0200

    First 2 problems

---
+++ 03.PrimitiveDataTypeAndVariable/01.DeclareVariable/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class DeclareVariable
+{
+    static void Main()
+    {
+        ushort num1 = 52130;
+        sbyte num2 = 115;
+        int num3 = 4825932;
+        sbyte num4 = 97;
+        short num5 = -10000;
+    }
+}
---
+++ 03.PrimitiveDataTypeAndVariable/02.FloatOrDouble/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class FloatOrDouble
+{
+    static void Main()
+    {
+        double num = 34.567839023;
+        float num1 = 12.345f;
+        double num2 = 8923.1234857;
+        float num3 = 3456.091f;
+
+        Console.WriteLine(num);
+        Console.WriteLine(num1);
+        Console.WriteLine(num2);
+        Console.WriteLine(num3);
+        Console.ReadLine();
+    }
+}
