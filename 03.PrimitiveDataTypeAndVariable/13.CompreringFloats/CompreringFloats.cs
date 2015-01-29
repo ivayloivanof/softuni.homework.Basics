fc7355c685ec0327c2b653c8e42d454d42c70368
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-01-29 23:09:17 +0200

    homework complited

---
+++ 03.PrimitiveDataTypeAndVariable/13.CompreringFloats/CompreringFloats.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class CompreringFloats
+{
+    static void Main()
+    {
+        decimal a = 8.654331m;
+        decimal b = 8.654321m;
+        decimal sum = (decimal)(a - b);
+        decimal eps = 0.000001m;
+        Console.WriteLine((a - b));
+
+        if ( sum < eps)
+        {
+            Console.WriteLine("True");
+        }
+        else
+        {
+            Console.WriteLine("False");
+        }
+        Console.ReadLine();
+    }
+}
---
+++ 03.PrimitiveDataTypeAndVariable/14.PrintTheAsciiTable/PrintTheAsciiTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class PrintTheAsciiTable
+{
+    static void Main()
+    {
+        int a = 1;
+        int i;
+
+        for (i = 0; i <= 250; i++)
+        {
+            Console.WriteLine(i + " = " + (char)i);
+        }
+        Console.ReadLine();
+    }
+}
