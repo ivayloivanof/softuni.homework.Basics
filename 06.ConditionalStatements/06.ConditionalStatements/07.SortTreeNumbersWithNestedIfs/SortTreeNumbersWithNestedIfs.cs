939e3a19dab873ca7bac5c226cc3614b52bdf722
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-02-22 00:08:23 +0200

    add conditional statements homework

---
+++ 06.ConditionalStatements/06.ConditionalStatements/07.SortTreeNumbersWithNestedIfs/SortTreeNumbersWithNestedIfs.cs
@@ -0,0 +1,46 @@
+﻿using System;
+class SortTreeNumbersWithNestedIfs
+{
+    static void Main()
+    {
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        double c = double.Parse(Console.ReadLine());
+
+        if ((a >= b) && (a >= c))
+        {
+            if (b >= c)
+            {
+                Console.WriteLine("{0} {1} {2}", a, b, c);
+            }
+            else if (b <= c)
+            {
+                Console.WriteLine("{0} {1} {2}", a, c, b);
+            }
+        }
+        else if ((b >= a) && (b >= c))
+        {
+            if (a >= c)
+            {
+                Console.WriteLine("{0} {1} {2}", b, a, c);
+            }
+            else if (a <= c)
+            {
+                Console.WriteLine("{0} {1} {2}", b, c, a);
+            }
+        }
+        else if ((c >= a) && (c >= b))
+        {
+            if (a >= b)
+            {
+                Console.WriteLine("{0} {1} {2}", c, a, b);
+            }
+            else if (a <= b)
+            {
+                Console.WriteLine("{0} {1} {2}", c, b, a);
+            }
+        }
+        Console.ReadLine();
+    }
+}
+
---
+++ 06.ConditionalStatements/06.ConditionalStatements/09.PlayWithIntDoubleAndString/PlayWithIntDoubleAndString.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class PlayWithIntDoubleAndString
+{
+    static void Main()
+    {
+        Console.WriteLine("Please choose a type: ");
+        Console.WriteLine("1 --> int");
+        Console.WriteLine("2 --> double");
+        Console.WriteLine("3 --> string");
+        int chouse = int.Parse(Console.ReadLine());
+
+        switch (chouse)
+        {
+            case 1:
+                Console.Write("Please enter a int: ");
+                int intN = int.Parse(Console.ReadLine());
+                Console.WriteLine(intN + 1);
+                break;
+            case 2:
+                Console.Write("Please enter a double: ");
+                double doubleN = double.Parse(Console.ReadLine());
+                Console.WriteLine(doubleN + 1);
+                break;
+            case 3:
+                Console.Write("Please enter a string: ");
+                string stringN = Console.ReadLine();
+                Console.WriteLine(stringN + "*");
+                break;
+        }
+        Console.ReadLine();
+    }
+}
